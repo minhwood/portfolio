52b7952f8a728e66ca0f2885aec1f855951cb247
Minh Doan Quang <dquangminh98@gmail.com>
2020-07-21 16:46:13 +0700

    AddRouting

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-  </div>
+    <div id="app">
+      <router-view></router-view>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
-  }
+    name: 'App',
+    components: {
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import router from './routes'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
