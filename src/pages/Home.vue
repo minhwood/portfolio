da06baec12f40fa49de256d9ccea029c2728da37
Minh Doan Quang <dquangminh98@gmail.com>
2020-07-22 12:10:27 +0700

    AddBackground

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <div class="dark-background background"></div>
+        <div class="light-background background"></div>
     </div>
 </template>
 
@@ -10,3 +12,27 @@ export default {
     }
 }
 </script>
+<style scoped>
+.background {
+  min-height: 100vh;
+  width: 50%;
+  height: 100%;
+  z-index: -1000;
+}
+
+.dark-background {
+  position: fixed;
+  left:0;
+  top: 0;
+  background-color: rgb(25, 34, 39);
+  display: inline-block;
+}
+
+.light-background {
+  position: fixed;
+  right: 0;
+  top: 0;
+  background-color: rgb(38, 50, 56);
+  display: inline-block;
+}
+</style>
